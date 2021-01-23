5e942136a5e2b8214f088181d4174ff8d4685587
Elad Karako <eladkarako@users.noreply.github.com>
2021-01-23 03:53:07 +0200

    adding align.sh and verify.sh, ignoring error messages for now - it should be removed. also an error handling (for all batch files) should be added, currently there is none.

--- resign.sh
+++ resign.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
-./clear_signature.sh "$1";
+./clear_signature.sh "$1"   2>/dev/null;
 
-./sign.sh "$1";
+./sign.sh "$1"              2>/dev/null;
+
+./align.sh "$1"             2>/dev/null;
+
+./verify.sh "$1"            2>/dev/null;
 
