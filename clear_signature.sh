d8757b7f48be29f800121f5b8acd3e05be946100
Elad Karako <eladkarako@users.noreply.github.com>
2021-01-23 03:45:37 +0200

    work within a temp. folder, use relative paths based on the target file full name and extension, avoid creating empty MANIFEST.MF file since jarsigner will not write its meta-data in an empty file (first line is empty) just the file-checksums - it still works fine but does not look nice, also favor deleting an empty MANIFEST.MF if possible (and avoid adding whitespace or newlines for the same reason above), by directly specify -tzip -mem=ZipCrypto -mm=Deflate, it is fine to write the zip-archive as the extension needed (jar or apk as well as zip), since 7z does not need to guess the algorithm and format this saves confusing renaming afterwards.

--- clear_signature.sh
+++ clear_signature.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-set -x
-set -e
+set +x
+set +e
 ###-----------------
 
 
@@ -12,43 +12,62 @@ BASE=$(basename "$BASE_EXT" ".$EXT"                               | head -1);   
 
 
 
+cd "$FULLY_QUALIFIED__PATH";
+pwd
 
-rm -rf "$FULLY_QUALIFIED__PATH/$BASE/"                                                ### pre-cleanup (old sessions?)
-7z x "$FULLY_QUALIFIED__PATH_BASE_EXT" -o"$FULLY_QUALIFIED__PATH/$BASE/";             ### extract content from archive, into a newly created folder in the given path.
+rm -rf "./_tmp_$BASE_EXT/";                                                ### pre-cleanup (old sessions?)
+7z x -y "./$BASE_EXT" -o"./_tmp_$BASE_EXT/";             ### extract content from archive, into a newly created folder in the given path.
 
-mkdir -p "$FULLY_QUALIFIED__PATH/$BASE/META-INF/";                                    ### make sure sub-folder is there.
-cd "$FULLY_QUALIFIED__PATH/$BASE/META-INF/"                                           ### prepare to clean 'META-INF' old-content but keeping some stuff.
+cd "./_tmp_$BASE_EXT/";
+pwd
+
+mkdir -p "META-INF";                                    ### make sure sub-folder is there.
+cd "./META-INF/";                                           ### prepare to clean 'META-INF' old-content but keeping some stuff.
+pwd
 
 ###-----------------------------------------------------------------------------------### remove certificates and checksums, keep other stuff ('Manifest-Version: 1.0', 'Created-By: 1.8.0_151 (Oracle Corporation)', 'Main-Class: input2stdout' - for example).
 rm -f *.RSA *.SF;
 
-
-sed -i -r '/^Name:/d'    "./MANIFEST.MF"  #lines starting with 'Name:'
-sed -i -r '/^SHA/d'      "./MANIFEST.MF"  #lines starting with 'SHA'
-sed -i -r '/Digest:/d'   "./MANIFEST.MF"  #lines with 'Digest:'
- 
-sed -i -r '/^\s*$/d'     "./MANIFEST.MF"  #empty-lines.
-dos2unix "./MANIFEST.MF"                  #to linux-EOL (\n).
-sed -i -r 's#\n$\n$#\n#' "./MANIFEST.MF"  #empty lines.
-echo "" >>"./MANIFEST.MF"                 #add one empty line at the end.
-unix2dos "./MANIFEST.MF"                  #to Windows-EOL (\r\n).
-
+##-----------------------------------only work on MANIFEST.MF if there is any, and avoiding creating a blank one with 'touch MANIFEST.MF' - it is bad since the jarsigner will not write headers such 'Created-By', etc.. - if there is an empty file already, it will just write file checksums, it will probably work, but it does not look as nice.
+if [ -f "./MANIFEST.MF" ]; then
+  sed -i -r '/^Name:/d'                "./MANIFEST.MF";  #lines starting with 'Name:'
+  sed -i -r '/^SHA/d'                  "./MANIFEST.MF";  #lines starting with 'SHA'
+  sed -i -r '/Digest:/d'               "./MANIFEST.MF";  #lines with 'Digest:'
+   
+  sed -i -r '/^\s*$/d'                 "./MANIFEST.MF";  #empty-lines.
+  dos2unix "./MANIFEST.MF";                              #unify to linux-EOL (\n) so the following regular-expressions will be simpler.
+  sed -i -r 's#\s*\n\s*$\s*\n\s*$#\n#' "./MANIFEST.MF";  #empty lines.
+  sed -i -r '/\s*\n\s*$/d'             "./MANIFEST.MF";  #empty-lines.
+  
+  ##-----------------------------------------------------it is best to remove an empty MANIFEST.MF file, jarsigner will recreate it (an empty 'META-INF/' folder is fine though.
+  if [ -s "./MANIFEST.MF" ]; then
+    rm -f "./MANIFEST.MF";
+  else
+    unix2dos "./MANIFEST.MF";                              #unify to Windows-EOL (\r\n) to be readable in Windows, and Windows-compiled binaries.
+  fi
+fi
 
 ###-----------------------------------------------------------------------------------###
 
-cd "$FULLY_QUALIFIED__PATH/$BASE/"
+cd ../../
+pwd
+
 ####-ssw
 
-mv --verbose --no-target-directory --force "$FULLY_QUALIFIED__PATH_BASE_EXT" "$FULLY_QUALIFIED__PATH/_original__$BASE_EXT"
+mv --verbose --no-target-directory --force "./$BASE_EXT" "./_original__$BASE_EXT"
 
-7z a -tzip -y -mmt4 -mx9 -mem=ZipCrypto -mm=Deflate "$FULLY_QUALIFIED__PATH/$BASE.zip" '*'
+cd "./_tmp_$BASE_EXT/";
+pwd
 
-mv --verbose --no-target-directory --force "$FULLY_QUALIFIED__PATH/$BASE.zip" "$FULLY_QUALIFIED__PATH/$BASE.$EXT"
+7z a -tzip -y -mmt4 -mx9 -mem=ZipCrypto -mm=Deflate "-x!./$BASE.$EXT" "./$BASE.$EXT" '*'
 
-rm -fr "$FULLY_QUALIFIED__PATH/$BASE/"
+##mv --verbose --no-target-directory --force "./$BASE.zip" "./$BASE.$EXT"
 
+mv --verbose --force "./$BASE.$EXT" "../";
 
-###-----------------
-set +x
-set +e
+cd "../";
+pwd
+
+rm -fr "./_tmp_$BASE_EXT/";
+
 
