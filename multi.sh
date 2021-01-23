539f9c8a9ff74dd69e9e6eae18dccfa241babfde
Elad Karako <eladkarako@users.noreply.github.com>
2021-01-23 04:07:26 +0200

    ooops.. forgot to include *.apk files... :]

--- multi.sh
+++ multi.sh
@@ -15,6 +15,6 @@ FULLY_QUALIFIED__RESIGN_SH="$FULLY_QUALIFIED__SELF_PATH/resign.sh"; ### fully qu
 
 ###find . -type f -name '*.zip' -name '*.jar' -name '*.apk' -print0 | parallel -0 unzip -d {/.} {}
 
-find "$FULLY_QUALIFIED__PATH_BASE_EXT" -type f \( -name "*.zip" -o -name "*.jar" -o -prune -name "_original_*" \) -print0 | parallel -0 "$FULLY_QUALIFIED__RESIGN_SH" {}
+find "$FULLY_QUALIFIED__PATH_BASE_EXT" -type f \( -name "*.zip" -o -name "*.jar" -o -name "*.apk" -o -prune -name "_original_*" \) -print0 | parallel -0 "$FULLY_QUALIFIED__RESIGN_SH" {}
 
 ##find "$FULLY_QUALIFIED__PATH_BASE_EXT" -type f \( -name "*.zip" -o -name "*.jar" -o -prune -name "_original_*" \) -print0 | parallel -0 ls {}
