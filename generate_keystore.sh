1751dc63fafa57b5d6c75d19042589e76acdd19c
Elad Karako <eladkarako@users.noreply.github.com>
2021-01-23 03:49:55 +0200

    removing debug/error break. relative paths were replaced with fully qualified paths, but it is not need and should be turned back after some testing with gnu-parallel to make sure the working in folders is fine.

--- generate_keystore.sh
+++ generate_keystore.sh
@@ -1,23 +1,23 @@
 #! /bin/sh
-set -x
-set -e
-#--------------------------------------------------------------------------------------------
-
-rm -f "./foo.keystore"
-
-$JAVA_HOME/bin/keytool  -genkeypair                                     \
-                        -alias      "alias_name"                        \
-                        -keyalg     "RSA"                               \
-                        -keysize    "2048"                              \
-                        -sigalg     "SHA1withRSA"                       \
-                        -validity   "10000"                             \
-                        -keypass    "111111"                            \
-                        -keystore   "./foo.keystore"                    \
-                        -storepass  "111111"                            \
-                        -dname      'CN=*, OU=*, O=*, L=*, S=*, C=*'     \
-                        -v
-
-#--------------------------------------------------------------------------------------------
 set +x
 set +e
+#--------------------------------------------------------------------------------------------
+
+FULLY_QUALIFIED__SELF_PATH=$(dirname $0         | head -1);         ### fully qualified path only, of this script. this will be used to build an
+
+rm -f "$FULLY_QUALIFIED__SELF_PATH/foo.keystore"
+
+###$JAVA_HOME/bin/
+
+keytool -genkeypair                                     \
+        -alias      "alias_name"                        \
+        -keyalg     "RSA"                               \
+        -keysize    "2048"                              \
+        -sigalg     "SHA1withRSA"                       \
+        -validity   "10000"                             \
+        -keypass    "111111"                            \
+        -keystore   "$FULLY_QUALIFIED__SELF_PATH/foo.keystore"                    \
+        -storepass  "111111"                            \
+        -dname      'CN=*, OU=*, O=*, L=*, S=*, C=*'    \
+        -v
 
--- sign.sh
+++ sign.sh
@@ -1,17 +1,16 @@
 #! /bin/sh
-set -x
-set -e
+set +x
+set +e
 ###-----------------
 
 FULLY_QUALIFIED__PATH_BASE_EXT=$(readlink -f $1 | head -1);
+FULLY_QUALIFIED__SELF_PATH=$(dirname $0         | head -1);
 
-if [ ! -f "./foo.keystore" ]; then
-  "./generate_keystore.sh"
+if [ ! -f "$FULLY_QUALIFIED__SELF_PATH/foo.keystore" ]; then
+  "$FULLY_QUALIFIED__SELF_PATH/generate_keystore.sh"
 fi
 
-"$JAVA_HOME/bin/jarsigner" -keystore "./foo.keystore" -storepass "111111" -keypass "111111" -digestalg "SHA1" -sigalg "SHA1withRSA" "-verbose:all" -strict "$FULLY_QUALIFIED__PATH_BASE_EXT" "alias_name"
+####"$JAVA_HOME/bin/
 
-###-----------------
-set +x
-set +e
+jarsigner -keystore "$FULLY_QUALIFIED__SELF_PATH/foo.keystore" -storepass "111111" -keypass "111111" -digestalg "SHA1" -sigalg "SHA1withRSA" "-verbose:all" "$FULLY_QUALIFIED__PATH_BASE_EXT" "alias_name"
 
